9eb46d2bb081e874052b786edb249122e1c1b749
vicky <vs161616@gmail.com>
2024-07-02 13:16:56 +0530

    changes in App file

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ function App(): JSX.Element {
     <Router>
       <LoaderProvider>
         <AuthProvider>
+          
           <InvoiceProvider>
             <Layout />
           </InvoiceProvider>
